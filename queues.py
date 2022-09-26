cd21d81ac6ed8cea514ca51ab19edf0c67b08bd9
ItsMrxNeutron <newminecraft555@gmail.com>
2022-09-26 14:49:24 +0700

    added queues.py, made chages to stacks.py

--- queues.py
+++ queues.py
@@ -6,54 +6,45 @@ from typing import List
 class Node:
     def __init__(self, data, next_ptr) -> None:
         self.data = data
-        self.next_ptr = next_ptr        # the focus here is on Stacks, I wont bother with pointers
+        self.next = next_ptr            # the focus is in Queues, I wont bother with pointers here
 
-
-class Stack:
+class Queue:
     def __init__(self) -> None:
-        """LIFO style queue"""
+        """FIFO style list"""
         self.node_pool: List[Node] = []
         self.top = -1
         self.free = 0
-
         self.MAX_NODES = 10
 
-    
-    def push(self, node:Node) -> None:
+    def enqueue(self, node:Node):
         if self.free == -1:
             raise ValueError('Max nodes achieved.')
         self.node_pool.append(node)
+        self.free += 1
         self.top += 1
-        if self.free < self.MAX_NODES:
-            self.free += 1
-        else:
+        if self.free >= self.MAX_NODES:
             self.free = -1
-
-    def pop(self) -> Node:
+        
+    def dequeue(self):
         if self.top == -1:
             raise IndexError
-        return_elm = self.node_pool[self.top]
-        del self.node_pool[self.top]            # .pop method can be used here, I am simply avoiding it.
+        return_val = self.node_pool[0]
+        del self.node_pool[0]           # pop can be used here, I am avoiding the use of it
         self.top -= 1
-        if self.free == -1:
-            self.free = self.top + 1
-        else:
-            self.free -= 1
-        return return_elm
+        self.free -= 1
+        return return_val
 
     def is_empty(self):
         return self.top == -1
     
     def is_full(self):
-        return self.top == self.MAX_NODES
+        return self.free == -1
 
     # The part below here is unimportant to exam board but is a nice touch to the code
 
     def __len__(self):
         return self.free
 
-s = Stack()
-print(len(s))
 
 
 
